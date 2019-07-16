ca68e6cf585edc3034154d621e94d556c31702df
Amit Tyagi <amitt6580@gmail.com>
2019-07-16 23:01:53 +0530

    update in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
 user = User.create(email: 'amitt6580@gmail.com`', encrypted_password: '$2y$10$uriswSFoZi7XeelT3g1lreDLupIeuKcaPh222lKIm.QDEsYkvBlva', name:'Amit Tyagi', admin:'true',created_at:'2019-07-16 13:37:27',updated_at:'2019-07-16 13:37:27')
-# Character.create(name: 'Luke', movie: movies.first)
