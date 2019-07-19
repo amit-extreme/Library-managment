bf36d2066cb3783a0f3778e0a3aa9f1887629053
Amit Tyagi <amitt6580@gmail.com>
2019-07-19 20:04:01 +0530

    add omniauth-facbook gem for login facebook functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_235557) do
+ActiveRecord::Schema.define(version: 2019_07_19_141718) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 2018_04_23_235557) do
     t.string "card_type"
     t.boolean "admin"
     t.boolean "subscribed"
+    t.string "provider"
+    t.string "uid"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
